30c2e71f644e85c7169489e6be04f54f10b952e5
mithun-ksms <mithunsurriyaks@gmail.com>
2026-07-16 10:46:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-// Update this once your backend is live on Render.
-// Example: "https://london-tourism-chatbot.onrender.com"
-const BACKEND_URL = "https://your-app-name.onrender.com";
+
+const BACKEND_URL = "https://git-journey-nt5e.onrender.com";
